c356202f360239b1ebbac265ac454e45b402eb26
Deepika Srivastava <dpka9509@gmain.com>
2025-11-19 16:24:36 +0530

    Added 19-11-25 work

---
+++ workshop/callback.js
@@ -0,0 +1,8 @@
+function processUserInput(callback){
+    const name="John";
+    callback(name);
+}
+
+processUserInput(function(name){
+    console.log("Hello "+name);
+});
---
+++ workshop/set.js
@@ -0,0 +1,7 @@
+setTimeout(()=>{
+    console.log("Hello");
+},0); 
+
+for(let index=0; index<10; index++){
+    console.log(`index : ${index}`);
+}
